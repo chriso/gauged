22d6a71ce80704e2edd996d2cb63dd69732605ec
Chris O'Hara <cohara87@gmail.com>
2014-03-11 20:02:14 -0700

    0.3.0

--- gauged/version.py
+++ gauged/version.py
@@ -4,6 +4,6 @@ https://github.com/chriso/gauged (MIT Licensed)
 Copyright 2014 (c) Chris O'Hara <cohara87@gmail.com>
 '''
 
-__version__ = '0.2.1'
+__version__ = '0.3.0'
 
 __version_info__ = tuple([ int(v) for v in __version__.split('.') ])
